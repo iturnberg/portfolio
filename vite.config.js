332c3c1e40ddc30a160dd272d495d4b2a1ac8dcd
iturnberg <164777317+iturnberg@users.noreply.github.com>
2024-06-09 09:42:09 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  base: "./",
   env: {
     VITE_PUBLIC_KEY: '3pBNtfkPVVbJvUn-t',
     VITE_SERVICE_ID: 'service_mvz9zhm',
