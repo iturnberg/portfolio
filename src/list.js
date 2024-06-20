adcdbbb54d654b81349a6cdb2d605ae79665aa83
iturnberg <164777317+iturnberg@users.noreply.github.com>
2024-06-20 14:49:30 -0400

    testing path changes

--- src/list.js
+++ src/list.js
@@ -9,28 +9,28 @@
 const list = [
   {
     name:'Pantry.js',
-    img:'/src/assets/pantrydotjs.png',
+    img:'../src/assets/pantrydotjs.png',
     description:'A pantry tracker/cookbook application built using React and GraphQL',
     repo:'https://github.com/LemonTag/Pantry.js',
     link:'https://pantry-js.onrender.com/',
 },
     {
       name: 'The Final Frontier',
-      img: '/src/assets/finalfrontier.png',
+      img: '../src/assets/finalfrontier.png',
 description: 'Website to view images from various NASA API`s. Built using HTML, JS, Jquery and CSS.',
       repo: 'https://github.com/RyanH118/The_Final_Frontier',
       link: 'https://ryanh118.github.io/The_Final_Frontier/',
     },
     {
       name: 'Personal Blog',
-      img: '/src/assets/blog.png',
+      img: '../src/assets/blog.png',
       description: 'Simple blog made using HTML, JS and CSS. Has a light/dark mode, and saves posts to local storage',
       repo: 'https://github.com/iturnberg/personalblog',
       link: 'https://iturnberg.github.io/personalblog',
     },
     {
         name:'Employee Payroll Tracker',
-        img:'/src/assets/payrolltracker.png',
+        img:'../src/assets/payrolltracker.png',
         description:'Simple payroll tracker built using JS/HTML. Uses localstorage to save data',
         repo:'https://github.com/iturnberg/EmployeePayrollTracker',
         link:'https://iturnberg.github.io/EmployeePayrollTracker/',
