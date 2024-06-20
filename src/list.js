9c330b1065f34cac51f70a53d6e9f5b630bd0e25
iturnberg <ian.r.turnberg@gmail.com>
2024-06-20 15:04:09 -0400

    if this doesnt work Im gunna start crying

--- src/list.js
+++ src/list.js
@@ -1,3 +1,8 @@
+import pantryImage from './assets/pantrydotjs.png';
+import finalFrontierImage from './assets/finalfrontier.png';
+import blogImage from './assets/blog.png';
+import payrollTrackerImage from './assets/payrolltracker.png';
+
 // {
 //     name:'',
 //     img:'',
@@ -9,32 +14,32 @@
 const list = [
   {
     name:'Pantry.js',
-    img:'../assets/pantrydotjs.png',
+    img: pantryImage,
     description:'A pantry tracker/cookbook application built using React and GraphQL',
     repo:'https://github.com/LemonTag/Pantry.js',
     link:'https://pantry-js.onrender.com/',
-},
-    {
-      name: 'The Final Frontier',
-      img: '../assets/finalfrontier.png',
-description: 'Website to view images from various NASA API`s. Built using HTML, JS, Jquery and CSS.',
-      repo: 'https://github.com/RyanH118/The_Final_Frontier',
-      link: 'https://ryanh118.github.io/The_Final_Frontier/',
-    },
-    {
-      name: 'Personal Blog',
-      img: '../assets/blog.png',
-      description: 'Simple blog made using HTML, JS and CSS. Has a light/dark mode, and saves posts to local storage',
-      repo: 'https://github.com/iturnberg/personalblog',
-      link: 'https://iturnberg.github.io/personalblog',
-    },
-    {
-        name:'Employee Payroll Tracker',
-        img:'../assets/payrolltracker.png',
-        description:'Simple payroll tracker built using JS/HTML. Uses localstorage to save data',
-        repo:'https://github.com/iturnberg/EmployeePayrollTracker',
-        link:'https://iturnberg.github.io/EmployeePayrollTracker/',
-    },
+  },
+  {
+    name: 'The Final Frontier',
+    img: finalFrontierImage,
+    description: 'Website to view images from various NASA API`s. Built using HTML, JS, Jquery and CSS.',
+    repo: 'https://github.com/RyanH118/The_Final_Frontier',
+    link: 'https://ryanh118.github.io/The_Final_Frontier/',
+  },
+  {
+    name: 'Personal Blog',
+    img: blogImage,
+    description: 'Simple blog made using HTML, JS and CSS. Has a light/dark mode, and saves posts to local storage',
+    repo: 'https://github.com/iturnberg/personalblog',
+    link: 'https://iturnberg.github.io/personalblog',
+  },
+  {
+    name:'Employee Payroll Tracker',
+    img: payrollTrackerImage,
+    description:'Simple payroll tracker built using JS/HTML. Uses localstorage to save data',
+    repo:'https://github.com/iturnberg/EmployeePayrollTracker',
+    link:'https://iturnberg.github.io/EmployeePayrollTracker/',
+  },
 //     {
 //     name:'Note Taker',
 //     img:'/src/assets/notetaker.png',
