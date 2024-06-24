c84ffbd5144e8c42ca70021e113f67b892d1a278
iturnberg <ian.r.turnberg@gmail.com>
2024-06-24 13:59:35 -0400

    adjusted about size, fixed the blue bar, updated pantry.js description

--- src/pages/bigGrid.jsx
+++ src/pages/bigGrid.jsx
@@ -2,33 +2,37 @@ import React from 'react';
 import { Grid, Box } from '@mui/material';
 import About from './about';
 import Contact from './contact';
-import Portfolio from './portfolio';
 import CardBelt from '../components/skills';
 import FlipSwitch from '../components/switch';
 
 const FullScreenGrid = () => {
+  const blackBackgroundStyle = {
+    backgroundColor: 'black',
+    color: 'white', // Optional: Set text color to white for better readability
+  };
+
   return (
     <Grid container style={{ height: '100vh', maxHeight: '100vh', overflow: 'hidden' }}>
-      <Grid item xs={6} style={{ backgroundColor: 'lightblue', display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Grid item xs={6} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', height: '100%' }}>
         <Grid container direction="column" style={{ flex: 1 }}>
-          <Grid item style={{ height: '35%', backgroundColor: 'black', display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+          <Grid item style={{ height: '30%', ...blackBackgroundStyle, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
             <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
               <About />
             </Box>
           </Grid>
-          <Grid item style={{ height: '15%', backgroundColor: 'black', display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+          <Grid item style={{ height: '15%', ...blackBackgroundStyle, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
             <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
               <CardBelt />
             </Box>
           </Grid>
-          <Grid item style={{ height: '50%', backgroundColor: 'black', display: 'flex', flexDirection: 'column'}}>
+          <Grid item style={{ height: '50%', ...blackBackgroundStyle, display: 'flex', flexDirection: 'column' }}>
             <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
               <Contact />
             </Box>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={6} style={{ backgroundColor: 'black', display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+      <Grid item xs={6} style={{ ...blackBackgroundStyle, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
         <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
           <FlipSwitch />
         </Box>
